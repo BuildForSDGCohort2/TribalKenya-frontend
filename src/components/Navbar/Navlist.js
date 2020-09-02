4c6ea977ef91d5e04e009e6973f632cbb1f055f9
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-09-02 12:18:44 +0300

    feat: add logo

--- src/components/Navbar/Navlist.js
+++ src/components/Navbar/Navlist.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BiSearchAlt } from 'react-icons/bi';
 import Navitem from './Navitem';
+import Brand from './Brand';
 
 const Navlist = () => {
   return (
         <ul className="nav-list">
+            <Navitem icon={<Brand/>} />
             <Navitem link="/" title="Home" />
             <Navitem link="/about" title="About" />
             <Navitem link="/sightseeing" title="Sightseeing" />

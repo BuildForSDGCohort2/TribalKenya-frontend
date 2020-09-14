e41fba942bc368009b540e4e93349f9f6140d269
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-09-14 18:18:59 +0300

    feat:add sample data for treks

---
+++ src/state/treks/treksData.js
@@ -0,0 +1,212 @@
+import trek1 from '../../images/trek-1.jpg';
+
+export const treksData = [
+  {
+    id: 1,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 2,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 3,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 4,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 5,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 6,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 7,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  },
+  {
+    id: 8,
+    username: 'Trigga Maserati',
+    trek: [
+      {
+        images: [trek1],
+        text: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi possimus corporis consectetur rerum incidunt dolore id dolorum quo ab qui!',
+        videos: []
+      }
+    ],
+    tags: '#wildlife',
+    category: 'Wildlife',
+    location: 'Nairobi National Park',
+    privacy: 'Public',
+    comments: [
+      { postId: 1, username: 'Mary', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Eliza', comment: 'Wow looking good', reports: [] },
+      { postId: 1, username: 'Trekstar', comment: 'Wow looking good', reports: [] }
+    ],
+    likes: [
+      { username: 'Mary' },
+      { username: 'Eliza' },
+      { username: 'Trekstar' }
+    ],
+    reports: []
+  }
+];

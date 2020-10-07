2cc08f2d97ac47621e751aa95f52bde7af4e7d7c
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-10-07 15:51:22 +0300

    fix:fix issue of async actiois due to lack of custom middleware
    add redux-thunk middleware
    add new async action for fetching categories from db

--- src/components/homepage/sightseeing-preview/SightSeeingPrev.js
+++ src/components/homepage/sightseeing-preview/SightSeeingPrev.js
@@ -3,29 +3,28 @@ import { connect } from 'react-redux';
 import AOS from 'aos';
 import { navigate } from '@reach/router';
 import TextContent from '../../TextContent';
-import { addpreviewSites } from '../../../state/sightseeing/sightseeing.actions';
+import { getCategories } from '../../../state/sightseeing/sightseeing.actions';
 import { stPrevData } from '../../../state/sightseeing/stData';
 import LargeBtn from '../../LargeBtn';
 
-const SightSeeingPrev = ({ previewSites, addpreviewSites }) => {
+const SightSeeingPrev = ({ siteCategories, getCategories }) => {
   const goToPage = () => navigate('sightseeing');
   useEffect(() => {
-    addpreviewSites(stPrevData);
+    getCategories(stPrevData);
     AOS.init({ once: true });
-  }, [addpreviewSites]);
+  }, [getCategories]);
   return (
       <>
         <section className="st-prev-sec w-100 p-3">
             <TextContent heading="Sightseeing" textColor="c-cream text-center" />
             <div className="gallery">
-                {previewSites.map((key) => (
+                {siteCategories.map((key) => (
                     <div className="gallery-item cursor"
                         key={key.name}
                         style={{ backgroundImage: `url(${key.poster})` }}
                         data-aos="zoom-in" data-aos-duration="500" >
                         <div className="gallery-text animate__animated animate__slideInDown">
                             <h1 className="medium-text animate__animated animate__rotateInDownLeft">{key.name}</h1>
-                            <p className="small-text animate__animated animate__rotateInUpRight">{key.location}</p>
                         </div>
                     </div>
                 ))}
@@ -37,11 +36,11 @@ const SightSeeingPrev = ({ previewSites, addpreviewSites }) => {
 };
 
 const mapStateToProps = (state) => ({
-  previewSites: state.sightSeeing.previewSites
+  siteCategories: state.sightSeeing.siteCategories
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  addpreviewSites: (previewSites) => dispatch(addpreviewSites(previewSites))
+  getCategories: (siteCategories) => dispatch(getCategories(siteCategories))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SightSeeingPrev);
